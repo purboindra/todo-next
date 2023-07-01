00ac00e391ea25a333f31e1faa4772eaf4df5194
purboindra <purboindra@gmail.com>
2023-07-01 23:29:02 +0700

    modal provider

---
+++ app/components/modals/ModalProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import RegisterModal from "./RegisterModal";
+
+const ModalProvider = () => {
+  return <RegisterModal />;
+};
+
+export default ModalProvider;
